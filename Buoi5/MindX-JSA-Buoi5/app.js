69a25d08a1d19b200b3491e2617b91108f7a6c5e
NguyenHuyTan-MindX <118376784+NguyenHuyTan-MindX@users.noreply.github.com>
2023-01-07 06:08:19 -0800

    Add files via upload

---
+++ Buoi5/MindX-JSA-Buoi5/app.js
@@ -0,0 +1,6 @@
+sessionStorage.setItem("name", "Quang Hao");
+//sessionStorage FORMAT
+let personName = sessionStorage.getItem("name");
+// let convatNawme/lophocName
+document.getElementById("Demo").innerHTML = personName
+//document.getElementById("id").innerHTML = doi tuoong vua tao (personName)
